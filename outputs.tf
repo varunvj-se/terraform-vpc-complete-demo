2d21f6b586d3aeeb510e8440a456c9b58de335b1
Varun Vijaywargi <varun.vijaywargi@caap.com.au>
2023-05-05 10:03:25 +1000

    Changes to output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "ec2_public_ips" {
   description = "Public IP of all EC2 instances"
-  value       = aws_instance.terraform-ec2-instance.*.public_ip
+  value       = aws_instance.terraform-ec2-instance[*].public_ip
 }
 
 output "ec2_public_dns" {
   description = "Public DNS of all EC2 instances"
-  value       = aws_instance.terraform-ec2-instance.*.public_dns
+  value       = aws_instance.terraform-ec2-instance[*].public_dns
 }
